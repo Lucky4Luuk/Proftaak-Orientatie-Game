2fa4ffc8f6813dabd28727dc3608326f5cd50435
Spheya <33225496+Spheya@users.noreply.github.com>
2019-10-28 14:50:56 +0100

    Added an entitymanager

---
+++ Proftaak-Orientatie-Game/Proftaak-Orientatie-Game/src/Entities/EntityManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using SFML.Graphics;
+
+namespace Proftaak_Orientatie_Game.Entities
+{
+    class EntityManager
+    {
+        private readonly List<IEntity> _entities = new List<IEntity>();
+
+        public void Add(IEntity entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public void Update(float deltaTime)
+        {
+            foreach(IEntity e in _entities)
+                e.OnUpdate(deltaTime);
+
+            DeleteMarkedEntities();
+        }
+
+        public void FixedUpdate(float fixedDeltaTime)
+        {
+            foreach (IEntity e in _entities)
+                e.OnFixedUpdate(fixedDeltaTime);
+
+            DeleteMarkedEntities();
+        }
+
+        public void Draw(float deltatime, RenderWindow window)
+        {
+            foreach (IEntity e in _entities)
+                e.OnDraw(deltatime, window);
+        }
+
+        private void DeleteMarkedEntities()
+        {
+            for (int i = 0; i < _entities.Count; i++)
+            {
+                if (_entities[i].HasDeletionMark())
+                {
+                    _entities.RemoveAt(i);
+                    --i;
+                }
+            }
+        }
+    }
+}
---
+++ Proftaak-Orientatie-Game/Proftaak-Orientatie-Game/src/Entities/IEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace Proftaak_Orientatie_Game.Entities
+{
+    abstract class IEntity
+    {
+        private bool _delete = false;
+
+        public abstract void OnUpdate(float deltatime);
+        public abstract void OnFixedUpdate(float fixedDeltatime);
+        public abstract void OnDraw(float deltatime, RenderWindow window);
+
+        public void MarkForDeletion()
+        {
+            _delete = true;
+        }
+
+        public bool HasDeletionMark()
+        {
+            return _delete;
+        }
+    }
+}
