9071026a4dfbadae99ba21f536df8ebd6934f279
Spheya <33225496+Spheya@users.noreply.github.com>
2019-10-28 15:28:02 +0100

    Adding some classes

---
+++ Proftaak-Orientatie-Game/Proftaak-Orientatie-Game/src/Entities/IPlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proftaak_Orientatie_Game.Entities
+{
+    class IPlayerController
+    {
+    }
+}
