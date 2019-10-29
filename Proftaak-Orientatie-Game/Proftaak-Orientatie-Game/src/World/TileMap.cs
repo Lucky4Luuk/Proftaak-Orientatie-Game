0508934f0525b0f985c760754a551747e94be93e
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-10-29 11:50:20 +0100

    hyesdjfweihrqwehfasdhio

--- Proftaak-Orientatie-Game/Proftaak-Orientatie-Game/src/World/TileMap.cs
+++ Proftaak-Orientatie-Game/Proftaak-Orientatie-Game/src/World/TileMap.cs
@@ -1,4 +1,5 @@
-﻿using SFML.Graphics;
+﻿using SFML.System;
+using SFML.Graphics;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -16,12 +17,17 @@ namespace Proftaak_Orientatie_Game.World
         private TileLayer play_tiles;
         private TileLayer roof_tiles;
 
+        private Sprite[,] sprites;
+
         private Map map;
+
+        private Texture tileset;
 
         public TileMap(string filename)
         {
             LoadTilemap(filename, out background_tiles, out play_tiles, out roof_tiles, out map);
 
+            /*
             for (int y = 0, i = 0; y < map.Height; y++)
             {
                 for (int x = 0; x < map.Width; x++, i++)
@@ -35,6 +41,37 @@ namespace Proftaak_Orientatie_Game.World
                     var tile = tileset[gid];
 
                     //window.Draw(tile_sprite);
+                }
+            }
+            */
+
+            /*
+            tilesets = new Dictionary<String, Texture>();//[map.Tilesets.Length];
+
+            int i = 0;
+            foreach (ITileset ts in map.Tilesets.OfType<ITileset>().ToArray())
+            {
+                String path = ts.ImagePath.Replace("..", "res");
+                Console.WriteLine(path);
+                //tilesets[i] = new Texture(path);
+                tilesets.Add(ts.Name, new Texture(path));
+                Console.WriteLine("Tileset loaded");
+                i++;
+            }
+            */
+
+            ITileset ts = map.Tilesets.First<ITileset>();
+            String path = ts.ImagePath.Replace("..", "res");
+            tileset = new Texture(path);
+
+            sprites = new Sprite[map.Width, map.Height];
+            for (int x = 0; x < map.Width; x++)
+            {
+                for (int y = 0; y < map.Height; y++)
+                {
+                    sprites[x,y] = new Sprite(tileset);
+                    sprites[x, y].Position = new Vector2f(x * map.CellWidth, y * map.CellHeight);
+                    sprites[x, y].TextureRect = new IntRect(0,0, ts.TileWidth, ts.TileHeight);
                 }
             }
         }
@@ -55,7 +92,22 @@ namespace Proftaak_Orientatie_Game.World
 
         public override void OnDraw(float deltatime, RenderWindow window)
         {
-            
+            for (int y = 0, i = 0; y < map.Height; y++)
+            {
+                for (int x = 0; x < map.Width; x++, i++)
+                {
+                    var gid = background_tiles.Data[i];
+                    if (gid == 0)
+                        continue;
+
+                    var tileset = map.Tilesets.Single(ts => gid >= ts.FirstGid && ts.FirstGid + ts.TileCount > gid);
+                    //Console.WriteLine(tileset.Name);
+                    Tile tile = tileset[gid];
+
+                    sprites[x, y].TextureRect = new IntRect(tile.Left, tile.Top, tileset.TileWidth, tileset.TileHeight);
+                    window.Draw(sprites[x,y]);
+                }
+            }
         }
     }
 }
