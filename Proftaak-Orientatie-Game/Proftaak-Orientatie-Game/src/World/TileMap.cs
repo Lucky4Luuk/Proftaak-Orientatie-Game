36cf4b8585f7ba656918a0c5a0a36c10763cf960
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-10-29 10:14:41 +0100

    uhh

--- Proftaak-Orientatie-Game/Proftaak-Orientatie-Game/src/World/TileMap.cs
+++ Proftaak-Orientatie-Game/Proftaak-Orientatie-Game/src/World/TileMap.cs
@@ -16,16 +16,34 @@ namespace Proftaak_Orientatie_Game.World
         private TileLayer play_tiles;
         private TileLayer roof_tiles;
 
+        private Map map;
+
         public TileMap(string filename)
         {
-            LoadTilemap(filename, out background_tiles, out play_tiles, out roof_tiles);
+            LoadTilemap(filename, out background_tiles, out play_tiles, out roof_tiles, out map);
+
+            for (int y = 0, i = 0; y < map.Height; y++)
+            {
+                for (int x = 0; x < map.Width; x++, i++)
+                {
+                    var gid = background_tiles.Data[i];
+                    if (gid == 0)
+                        continue;
+
+                    var tileset = map.Tilesets.Single(ts => gid >= ts.FirstGid && ts.FirstGid + ts.TileCount > gid);
+                    Console.WriteLine(tileset.ImagePath.Replace("..", "res"));
+                    var tile = tileset[gid];
+
+                    //window.Draw(tile_sprite);
+                }
+            }
         }
 
-        static void LoadTilemap(string filename, out TileLayer background_tiles, out TileLayer play_tiles, out TileLayer roof_tiles)
+        static void LoadTilemap(string filename, out TileLayer background_tiles, out TileLayer play_tiles, out TileLayer roof_tiles, out Map map)
         {
             using (var stream = File.OpenRead(filename))
             {
-                var map = Map.FromStream(stream, ts => File.OpenRead(Path.Combine(Path.GetDirectoryName(filename), ts.Source)));
+                map = Map.FromStream(stream, ts => File.OpenRead(Path.Combine(Path.GetDirectoryName(filename), ts.Source)));
 
                 //Quick hack for now. Perhaps we want to change this in the future so we can have more layers that are picked based on a tag or something
                 TileLayer[] layers = map.Layers.OfType<TileLayer>().ToArray();
@@ -37,7 +55,7 @@ namespace Proftaak_Orientatie_Game.World
 
         public override void OnDraw(float deltatime, RenderWindow window)
         {
-
+            
         }
     }
 }
